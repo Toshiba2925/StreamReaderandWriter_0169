5761decea88a55f14d554edcf93d3e91a53b27a6
Toshiba2925 <aqifashiba25@gmail.com>
2025-06-09 15:20:28 +0700

    menambah try catch  exeption

--- manualExeption.cpp
+++ manualExeption.cpp
@@ -2,5 +2,19 @@
 using namespace std;
 
 int main() {
-  
+    try {
+        cout << "Selamat Belajar di Prodi TI UMY" << endl;
+        throw 3.5; //melemparkan sebuah integer maka
+        cout << "Pernyataan tidak akan di eksekusi" << endl;
+    }
+    catch (int a) {
+        //blok ini akan di eksekusi
+        cout<< a << " : Pengecualian akan dieksekusi" << endl;
+    }
+    catch (...) {
+        /*jika selain integer maka block ini akan dieksekusi*/
+        cout << "Default pengecualian dieksekusi" << endl;
+    }
+
+    return 0;
 }
